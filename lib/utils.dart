36490ca6bcb9beee0b1d3cffa46dd8be4ea4189b
david <woei66@gmail.com>
2024-12-04 09:58:23 +0800

    I agree to the CLA, CLA license

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter/foundation.dart';
 import 'package:crypto/crypto.dart';
